1271e3defce32673e293c712ac8d138a5bcbd0e3
Ernesto <78241327+thesarfo@users.noreply.github.com>
2023-03-11 19:45:46 +0000

    Update ug_grading_system.py

--- ug_grading_system.py
+++ ug_grading_system.py
@@ -5,20 +5,20 @@ score = float(input('What was your score? '))
 
 #conditions that show the user's grade based on their score
 if score > 80:
-    print('Your name is', name, 'You had an A in', course)
+    print('Name:', name, 'You had an A in', course)
 elif score >= 75 and  score <=79:
-    print('Your name is', name, 'You had a B+ in', course)
+    print('Name:', name, 'You had a B+ in', course)
 elif score >= 70 and  score <=74:
-    print('Your name is', name, 'You had a B in', course)
+    print('Name:', name, 'You had a B in', course)
 elif score >= 65 and  score <=69:
-    print('Your name is', name, 'You had a C+ in', course)
+    print('Name:', name, 'You had a C+ in', course)
 elif score >= 60 and  score <=64:
-    print('Your name is', name, 'You had a C in', course)
+    print('Name:', name, 'You had a C in', course)
 elif score >= 55 and  score <=59:
-    print('Your name is', name, 'You had a D+ in', course)
+    print('Name:', name, 'You had a D+ in', course)
 elif score >= 50 and  score <=54:
-    print('Your name is', name, 'You had a D in', course)
+    print('Name:', name, 'You had a D in', course)
 elif score >= 45 and  score <=50:
-    print('Your name is', name, 'You had an E in', course)
+    print('Name:', name, 'You had an E in', course)
 else:
-    print('Your name is', name, 'You had an F in', course)
+    print('Name:', name, 'You had an F in', course)
