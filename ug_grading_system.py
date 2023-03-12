aab689be3a52c82ccbf600aa6ccefacb97b271d0
Ernesto <78241327+thesarfo@users.noreply.github.com>
2023-03-12 20:37:07 +0000

    Update ug_grading_system.py
    This program takes a user's name, course and score and tells them the grade they got in that score. It follows the University of Ghana's grading system

--- ug_grading_system.py
+++ ug_grading_system.py
@@ -1,24 +1,29 @@
-#defining variables for the users name, course and score
-name = input('Enter your name: ')
-course = input('Enter your course: ')
-score = float(input('What was your score? '))
+while True:
+    name = input('Enter your name: ')
+    course = input('Enter your course: ')
+    score = float(input('What was your score? '))
 
-#conditions that show the user's grade based on their score
-if score > 80:
-    print('Name:', name, 'You had an A in', course)
-elif score >= 75 and  score <=79:
-    print('Name:', name, 'You had a B+ in', course)
-elif score >= 70 and  score <=74:
-    print('Name:', name, 'You had a B in', course)
-elif score >= 65 and  score <=69:
-    print('Name:', name, 'You had a C+ in', course)
-elif score >= 60 and  score <=64:
-    print('Name:', name, 'You had a C in', course)
-elif score >= 55 and  score <=59:
-    print('Name:', name, 'You had a D+ in', course)
-elif score >= 50 and  score <=54:
-    print('Name:', name, 'You had a D in', course)
-elif score >= 45 and  score <=50:
-    print('Name:', name, 'You had an E in', course)
-else:
-    print('Name:', name, 'You had an F in', course)
+    # conditions that show the user's grade based on their score
+    if score > 80:
+        print('Name:', name, ' Course', course, ' Grade: A')
+    elif score >= 75 and  score <=79:
+        print('Name:', name, ' Course', course, ' Grade: B+')
+    elif score >= 70 and  score <=74:
+        print('Name:', name, ' Course', course, ' Grade: B')
+    elif score >= 65 and  score <=69:
+        print('Name:', name, ' Course', course, ' Grade: C+')
+    elif score >= 60 and  score <=64:
+        print('Name:', name, ' Course', course, ' Grade: C')
+    elif score >= 55 and  score <=59:
+        print('Name:', name, ' Course', course, ' Grade: D+')
+    elif score >= 50 and  score <=54:
+        print('Name:', name, ' Course', course, ' Grade: D')
+    elif score >= 45 and  score <=50:
+        print('Name:', name, ' Course', course, ' Grade: E')
+    else:
+        print('Name:', name, ' Course', course, ' Grade: F')
+
+    # ask user if they want to continue
+    choice = input('Do you want to enter another score? (y/n): ')
+    if choice.lower() == 'n':
+        break
