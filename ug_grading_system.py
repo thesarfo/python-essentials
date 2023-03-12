db0732f0537adee6ae6ae0659717d66f65436615
Ernesto <78241327+thesarfo@users.noreply.github.com>
2023-03-12 20:44:11 +0000

    Create ug_grading_system.py

--- ug_grading_system.py
+++ ug_grading_system.py
@@ -5,23 +5,23 @@ while True:
 
     # conditions that show the user's grade based on their score
     if score > 80:
-        print('Name:', name, ' Course', course, ' Grade: A')
+        print('Name:', name, ' Course:', course, ' Grade: A')
     elif score >= 75 and  score <=79:
-        print('Name:', name, ' Course', course, ' Grade: B+')
+        print('Name:', name, ' Course:', course, ' Grade: B+')
     elif score >= 70 and  score <=74:
-        print('Name:', name, ' Course', course, ' Grade: B')
+        print('Name:', name, ' Course:', course, ' Grade: B')
     elif score >= 65 and  score <=69:
-        print('Name:', name, ' Course', course, ' Grade: C+')
+        print('Name:', name, ' Course:', course, ' Grade: C+')
     elif score >= 60 and  score <=64:
-        print('Name:', name, ' Course', course, ' Grade: C')
+        print('Name:', name, ' Course:', course, ' Grade: C')
     elif score >= 55 and  score <=59:
-        print('Name:', name, ' Course', course, ' Grade: D+')
+        print('Name:', name, ' Course:', course, ' Grade: D+')
     elif score >= 50 and  score <=54:
-        print('Name:', name, ' Course', course, ' Grade: D')
+        print('Name:', name, ' Course:', course, ' Grade: D')
     elif score >= 45 and  score <=50:
-        print('Name:', name, ' Course', course, ' Grade: E')
+        print('Name:', name, ' Course:', course, ' Grade: E')
     else:
-        print('Name:', name, ' Course', course, ' Grade: F')
+        print('Name:', name, ' Course:', course, ' Grade: F')
 
     # ask user if they want to continue
     choice = input('Do you want to enter another score? (y/n): ')
